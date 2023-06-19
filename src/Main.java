dcc101f2390fcf12725e27d345fb12e9687a6d96
Olya france <francuzova_olya@mail.ru>
2023-06-19 23:41:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_386;
+
+        int miles = price / 20;
+
+
+
+                        System.out.println("Количество миль: " + miles + " миль");
+    }
+}
